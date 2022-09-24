1e41f737f33c76027812efa9029b7a6431801434
sdott3 <sdotthomas347@gmail.com>
2022-09-24 14:26:17 -0400

    starter files added

---
+++ db/schema.sql
@@ -0,0 +1,5 @@
+/* create and connect to the database */
+
+DROP DATABASE IF EXISTS tech_blog_db;
+
+CREATE DATABASE tech_blog_db;
---
+++ server.js
@@ -0,0 +1,7 @@
+const path = require('path');
+
+
+
+
+
+app.use(express.static(path.join(__dirname, 'public')));
